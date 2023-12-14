33e756398bc819ff0025222c5d42d252faa28a14
prakharpratapsingh21 <142224254+prakharpratapsingh21@users.noreply.github.com>
2023-12-14 20:05:44 +0530

    Add files via upload

---
+++ Day-3/Day-3.js
@@ -0,0 +1,39 @@
+
+//Write the Code for all Data types..
+
+//number
+var a=10;
+var b=10.5;
+console.log(a);
+console.log(typeof(a));
+
+
+//strings
+ let name="Prakhar";
+ console.log(name);
+ console.log(typeof(name));
+
+
+ //Booleans
+ let x=true;
+ let y=false;
+ console.log(typeof(x));
+
+ //Object
+ const person = {Fname: "Prakhar" ,Mname:"Pratap", Lname:"Singh"};
+ console.log(person);
+ console.log(typeof(person));
+
+ //Undefined
+ var z;
+ console.log(z);
+ console.log(typeof(z));
+
+ //Null
+ var c = null;
+ console.log(c);
+ console.log(typeof(c));
+
+ // Symbol
+const sym = Symbol('uniqueSymbol');
+console.log(typeof(sym));
